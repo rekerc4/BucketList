0b6f7ea3a002aa559021ae2bf780cfe150fe66c6
Ben Vasko <BenVasko@Bens-MacBook-Pro.local>
2018-08-22 16:24:55 -0400

    modal is working and will close

--- components/bucketlist/bucket.js
+++ components/bucketlist/bucket.js
@@ -9,6 +9,17 @@ const bucketlistPage = {
         {{info.name}} {{info.dates.start.localDate}} {{info._embedded.venues[0].name}} <a href="{{info.url}}">View on Ticketmaster</a>
         </section>
         <button ng-click="$ctrl.delete($index);"><i class="material-icons">close</i></button>
+        <button ng-click="$ctrl.moreInfo($index);">More Information</button>
+
+        <section class="modal" ng-if="$ctrl.show === $index">
+            <img src="{{ info.images[0].url }}">
+            <h1>Name: {{ info.name }}</h1>
+            <p>Date: {{ info.dates.start.localDate }}</p>
+            <p>Venue: {{ info._embedded.venues[0].name }}</p>
+            <a href="{{info.url}}">View on Ticketmaster</a>
+            <button class="favBtn" ng-click="$ctrl.favClick(info);"><i class="material-icons">star</i></button>
+            <button ng-click="$ctrl.closeInfo();"><i class="material-icons">clear</i></button>
+        </section>
         </li>
     </ul>
     `,
@@ -17,6 +28,12 @@ const bucketlistPage = {
         vm.list = TicketService.bucketlist;
         vm.delete = (index) => {
             vm.list.splice(index, 1);
+        }
+        vm.moreInfo = (index) => {
+            vm.show = index;
+        }
+        vm.closeInfo = () => {
+            vm.show = null;
         }
     }]
 }
--- components/eventlist/event.js
+++ components/eventlist/event.js
@@ -2,22 +2,43 @@
 
 let event = {
     bindings: "",
-    template: `<section class="eventList" ng-repeat="info in $ctrl.searchInfo track by $index">
-    <img src="{{info.images[0].url}}">
-  <h1>Name: {{ info.name }}</h1>
-  <p>Date: {{ info.dates.start.localDate }}</p>
-  <p>Venue: {{ info._embedded.venues[0].name }}</p>
-  <a href="{{info.url}}">View on Ticketmaster</a>
-  <button class="favBtn" ng-click="$ctrl.favClick(info);"><i class="material-icons">star</i></button>
-  </section>`,
+    template: `
+    <section class="eventList" ng-repeat="info in $ctrl.searchInfo track by $index">
+        <img src="{{ info.images[0].url }}">
+        <h1>Name: {{ info.name }}</h1>
+        <p>Date: {{ info.dates.start.localDate }}</p>
+        <p>Venue: {{ info._embedded.venues[0].name }}</p>
+        <a href="{{info.url}}">View on Ticketmaster</a>
+        <button class="favBtn" ng-click="$ctrl.favClick(info);"><i class="material-icons">star</i></button>
+        <button ng-click="$ctrl.moreInfo($index);">More Information</button>
+
+        <section class="modal" ng-if="$ctrl.show === $index">
+            <img src="{{ info.images[0].url }}">
+            <h1>Name: {{ info.name }}</h1>
+            <p>Date: {{ info.dates.start.localDate }}</p>
+            <p>Venue: {{ info._embedded.venues[0].name }}</p>
+            <a href="{{info.url}}">View on Ticketmaster</a>
+            <button class="favBtn" ng-click="$ctrl.favClick(info);"><i class="material-icons">star</i></button>
+            <button ng-click="$ctrl.closeInfo();"><i class="material-icons">clear</i></button>
+        </section>
+    </section>
+    `,
     controller: ["TicketService", function (TicketService) {
     const vm = this;
+    vm.show = null;
     vm.searchInfo = TicketService.getObject();
     vm.favClick = (index) => {
     TicketService.bucketlist.push(index);
     }
+    vm.moreInfo = (index) => {
+        vm.show = index;
     }
-    ]
+    vm.closeInfo = () => {
+        vm.show = null;
+    }
+
+    }]
+    
 }
 
 angular
