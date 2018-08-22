36887562f77e54481d2e1a659c15f22e505c1cbc
Kenneth <rekerc4@gmail.com>
2018-08-22 11:01:07 -0400

    ready for merge

--- services/api.js
+++ services/api.js
@@ -1,13 +1,61 @@
 "use strict";
 
-function TicketService($http) {
+function TicketService($http, ) {
     let vm = this;
-    vm.requestData = () => {
+    vm.requestData = (inter, city, fdat, ldat) => {
+        console.log(fdat, ldat);
+        console.log(inter + " " + city + " " + fdat + " " + ldat);
+        let interUri = "";
+        let cityUri = "";
+        let objec = null; 
+        function pad(number) {
+            if (number < 10) {
+              return '0' + number;
+            }
+            return number;
+          }
+      
+          Date.prototype.toISOString = function() {
+            return this.getUTCFullYear() +
+              '-' + pad(this.getUTCMonth() + 1) +
+              '-' + pad(this.getUTCDate()) +
+              'T' + pad(this.getUTCHours()) +
+              ':' + pad(this.getUTCMinutes()) +
+              ':' + pad(this.getUTCSeconds()) +
+              '.' + (this.getUTCMilliseconds() / 1000).toFixed(2).slice(2, 5) +
+              'Z';
+          };
+        let nfdat = fdat.toISOString().replace(/["."]+/g, ':');
+        console.log(nfdat); 
+        let nldat = encodeURIComponent(ldat.toISOString());  
+        console.log(fdat.toISOString());
+        if(typeof inter === "string" && typeof city === "string"){
+            interUri = encodeURIComponent(inter);
+            cityUri = encodeURIComponent(city);
+           console.log(cityUri);
+        }
+        else{
+            console.log('error'); 
+            return;
+        }
+
+        
         return $http({
         method: "GET",
-        url: "https://app.ticketmaster.com/discovery/v2/events.json?apikey=U7tG9w7O8UpfeSNk3oaR43EUFk1rMyoA"
+        url: `https://app.ticketmaster.com/discovery/v2/events.json?keyword=${interUri}&city=${cityUri}&apikey=U7tG9w7O8UpfeSNk3oaR43EUFk1rMyoA`
+        }).then((response) => {
+            objec = response.data;
+            console.log(objec._embedded.events);
+            //$location.path();
+            return objec;
         });
     };
+    vm.updateObject = (obj) => {
+        objec = obj;
+    }
+    vm.getObject = () => {
+        return objec;
+    }
 }
 
 
